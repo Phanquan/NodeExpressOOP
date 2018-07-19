aac1879cdb7ede66622acd61dbe47c2beb91004d
Phanquan <phanquan.2401@gmail.com>
2018-07-19 21:22:16 +0700

    "update"

--- route/GitHubRoute.js
+++ route/GitHubRoute.js
@@ -2,22 +2,30 @@ const express = require('express');
 const route = express.Router();
 const SimpleData = require('../model/SimpleData');
 const axios = require('axios');
+const GitHubService = require('../service/GitHubService');
 
-const GITHUB_INFO = '/:userName'
-let GET_USERNAME_URL = 'https://api.github.com/users';
+const GITHUB_INFO = '/:userName';
+
+const service = new GitHubService();
 
 route.get(GITHUB_INFO, (req, res, next) => {
     let userName = req.params.userName;
     let data = new SimpleData();
-    axios.get(GET_USERNAME_URL + '/' + userName)
-        .then(response => {
-            data.setData = response.data;
-            data.setMessage = `GET ${userName} Github Info Successfull`;
+    service.getUserInfo(userName)
+        .then((result) => {
+            data.setData = result.data;
             data.setSuccess = true;
+            data.setMessage = `GET ${userName} info successfully.`;
+            res.json(data);
+        }, (error) => {
+            data.setData = error.response.data;
+            data.setSuccess = false;
+            data.setMessage = `GET ${userName} info unsuccessfully.`;
             res.json(data);
         })
-        .catch(error => {
-            console.log(error);
+        .catch(err => {
+            console.log(err);
+            next();
         });
 });
 
